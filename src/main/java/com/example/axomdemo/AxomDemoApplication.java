f02ec6503255e110dba617415160dc8f9a42389f
Venkat <venkata_kalyan_krishna_varma_sagi@homedepot.com>
2020-07-13 22:09:58 -0400

    initial project setup

---
+++ src/main/java/com/example/axomdemo/AxomDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.axomdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxomDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxomDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/axomdemo/AxomDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.axomdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxomDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
